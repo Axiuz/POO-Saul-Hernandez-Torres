525f7ebeb358263ff83e5ec072d501bcfa463993
Axiuz <contactosaulhts@gmail.com>
2025-01-14 11:58:52 -0600

    primer commit

---
+++ POO/Proyecto/proyect-poo/src/main/java/edu/saul/hernandez/ACTIVIDADES/actividad1/starter.java
@@ -0,0 +1,9 @@
+package edu.saul.hernandez.ACTIVIDADES.actividad1;
+
+import edu.saul.hernandez.ACTIVIDADES.actividad1.ui.CLI;
+
+public class starter {
+public static void main(String[] args) {
+    CLI.ShowMenu();
+}
+}
---
+++ POO/Proyecto/proyect-poo/src/main/java/edu/saul/hernandez/ACTIVIDADES/actividad1/ui/CLI.java
@@ -0,0 +1,12 @@
+package edu.saul.hernandez.ACTIVIDADES.actividad1.ui;
+
+public class CLI {
+
+    public static void ShowMenu (){
+        System.out.println("La fokin app del saul");
+        System.out.println("1 siquesi");
+        System.out.println("2 noqueno");
+        
+
+    }
+}
