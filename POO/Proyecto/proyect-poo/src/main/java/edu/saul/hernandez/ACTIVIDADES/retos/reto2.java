fe19593ef967d86863e65ca53aea0687ef761101
Axiuz <contactosaulhts@gmail.com>
2025-01-21 11:47:08 -0600

    retos 2 y 3

--- POO/Proyecto/proyect-poo/src/main/java/edu/saul/hernandez/ACTIVIDADES/retos/reto2.java
+++ POO/Proyecto/proyect-poo/src/main/java/edu/saul/hernandez/ACTIVIDADES/retos/reto2.java
@@ -5,12 +5,23 @@ import java.util.Map;
 
 public class reto2 {
     public static void main(String[] args) {
-        string texto = "El rey de Parangaricutirimícuaro se quiere desparangaricutirimicuarizar, el que lo desparangaricutirimicuarize un buen desparangaricutirimicuarizador será";
+        String texto = "El rey de Parangaricutirimícuaro se quiere desparangaricutirimicuarizar, el que lo desparangaricutirimicuarize un buen desparangaricutirimicuarizador será";
         encontrarDuplicado(texto);
-    }
+            }
 
-    public static void encontrarDuplicado(String Texto){
         
-    }
+            public static void encontrarDuplicado(String texto) {
+        Map<Character, Integer> mapaFrecuencia = new HashMap<>();
 
+        for (char c : texto.toCharArray()) {
+            mapaFrecuencia.put(c, mapaFrecuencia.getOrDefault(c, 0) + 1);
+        }
+
+        System.out.println("Caracteres duplicados:");
+        for (Map.Entry<Character, Integer> entrada : mapaFrecuencia.entrySet()) {
+            if (entrada.getValue() > 1) {
+                System.out.println(entrada.getKey() + " aparece " + entrada.getValue() + " veces.");
+            }
+        }
+    }
 }
---
+++ POO/Proyecto/proyect-poo/src/main/java/edu/saul/hernandez/ACTIVIDADES/retos/reto3.java
@@ -0,0 +1,29 @@
+package edu.saul.hernandez.ACTIVIDADES.retos;
+
+public class reto3 {
+    public static void main(String[] args) {
+        int[] numeros = {34, 15, 88, 24221, 198688823, 762346783, 4324342};
+    
+        int minimo = encontrarMinimo(numeros);
+    
+            System.out.println("El valor más pequeño en el array es: " + minimo);
+        }
+    
+        public static int encontrarMinimo(int[] array) {
+            if (array.length == 0) {
+                throw new IllegalArgumentException("El array no puede estar vacío.");
+            }
+    
+            int minimo = array[0];
+
+            for (int num : array) {
+                if (num < minimo) {
+                    minimo = num;
+                }
+            }
+    
+            return minimo;
+        }
+    }
+    
+
