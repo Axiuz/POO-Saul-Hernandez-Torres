0ddfe060ed2a063f3b52574d30c598b285a4809f
Axiuz <contactosaulhts@gmail.com>
2025-01-23 18:33:33 -0600

    commin intellij

--- POO/Proyecto/proyect-poo/src/main/java/edu/saul/hernandez/actividades/retos/reto3.java
+++ POO/Proyecto/proyect-poo/src/main/java/edu/saul/hernandez/actividades/retos/reto3.java
@@ -2,7 +2,7 @@ package edu.saul.hernandez.ACTIVIDADES.retos;
 
 public class reto3 {
     public static void main(String[] args) {
-        int[] numeros = {34, 15, 88, 24221, 198688823, 762346783, 4324342};
+        int[] numeros = {34342523, 1579923859, 69, 24221, 198688823, 762346783, 4324342};
     
         int minimo = encontrarMinimo(numeros);
     
